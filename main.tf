039dbeac86a3a1b2ed2193ad95593ac6b2f06886
Daniel-Rosales-DevOps <danielrosales.devops@gmail.com>
2026-02-13 15:18:40 +0100

    code

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.60.0"
+    }
+  }
+
+  backend "azurerm" {
+    use_oidc         = true
+    use_azuread_auth = true
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-hello-world"
+  location = "EUROPE WEST"
+}
